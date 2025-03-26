d6cad817c506ad6373fcfa96e671eb6e757dca66
akhil <akhil@AKHIL>
2025-03-26 13:31:34 +0530

    unwanted mysqlconnections removed and changes made in insurance dao

--- InsuranceProject/src/main/java/com/InsuranceProject/controller/InsuranceServlet.java
+++ InsuranceProject/src/main/java/com/InsuranceProject/controller/InsuranceServlet.java
@@ -2,13 +2,13 @@ package com.InsuranceProject.controller;
 
 import java.io.IOException;
 
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.InsuranceProject.bean.HealthBean;
 import com.InsuranceProject.bean.VehicleBean;
@@ -90,7 +90,7 @@ public class InsuranceServlet extends HttpServlet {
 		a.setVehicle_Model(Vehicle_Model);
 		
 	    VehicleDao vd = new VehicleDao();
-	    int result = VehicleDao insertData(a);
+	    int result = VehicleDao insertData1(a);
 	    
 	    HttpSession session = request.getSession(); // keeping the session open
 		(session).setAttribute("VehicleObject", a);
--- InsuranceProject/src/main/java/com/InsuranceProject/dao/MySqlConnection.java
+++ InsuranceProject/src/main/java/com/InsuranceProject/dao/MySqlConnection.java
@@ -1,18 +1,18 @@
-package com.Insurance.dao;
+package com.InsuranceProject.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class MySqlConnection1 {
+public class MySqlConnection {
 
-	private static MySqlConnection1 mysql = new MySqlConnection1();
+	private static MySqlConnection mysql = new MySqlConnection();
 	Connection con;
 	
-	private MySqlConnection1() {
+	private MySqlConnection() {
 		
 		try {
 			 Class.forName("com.mysql.cj.jdbc.Driver");
-			 con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Health_Insurance","root","root");
+			 con = DriverManager.getConnection("jdbc:mysql://localhost:3306/insurance","root","root");
 			
 		} catch (Exception e) {
 		
@@ -26,7 +26,7 @@ public class MySqlConnection1 {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(MySqlConnection1.getInstance());
+		System.out.println(MySqlConnection.getInstance());
 	}
 
 }
